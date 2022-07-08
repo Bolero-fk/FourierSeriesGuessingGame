2f251ca9754d1923eee1f92490a202141f5a5b86
FukudaKento <fuku_0000@outlook.jp>
2022-07-08 21:30:52 +0900

    フーリエ関数を描画

---
+++ DrawFourier.js
@@ -0,0 +1,41 @@
+function DrawFourier() {
+}
+
+function GetFrourier(a1, a2, a5, a10, a20) {
+    var values = [];
+    for (var i = 0; i <= 1000; i++) {
+        var x = i / (1000) * 2 * Math.PI;
+        y = a1 * Math.sin(x) + 2 * a2 * Math.sin(2 * x) + 5 * a5 * Math.sin(5 * x)
+            + 10 * a10 * Math.sin(10 * x) + 20 * a20 * Math.sin(20 * x);
+        value = { x: x, y: y };
+        values.push(value);
+    }
+
+    return values;
+}
+
+function InitializeGraph() {
+    GetFrourier();
+    var ctx = document.getElementById('ex_chart');
+
+    var data = {
+        datasets: [{
+            radius: 0,
+            label: "フーリエ関数",
+            data: GetFrourier(1, 1, 1, 1, 1),
+            showLine: true,
+            fill: false,
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(255, 99, 132)'
+        }],
+    };
+
+    var config = {
+    };
+
+    var ex_chart = new Chart(ctx, {
+        type: 'scatter',
+        data: data,
+        options: config
+    });
+}
