85c953cf12f5daf7af0d6e0912120642c35e0a8f
FukudaKento <fuku_0000@outlook.jp>
2022-08-11 15:09:57 +0900

    連続した点はつなぐように変更

--- DrawFourier.js
+++ DrawFourier.js
@@ -7,9 +7,21 @@ function GetFrourier(a1, a5, a10, a25, a50) {
     var values = [];
     for (var i = 0; i <= 1000; i++) {
         var x = i / (1000) * 2 * Math.PI;
-        y = a1 * Math.sin(x) + a5 * Math.sin(5 * x) + a10 * Math.sin(10 * x)
+        var y = a1 * Math.sin(x) + a5 * Math.sin(5 * x) + a10 * Math.sin(10 * x)
             + a25 * Math.sin(25 * x) + a50 * Math.sin(50 * x);
-        value = { x: x, y: y };
+        var value = { x: x, y: y };
+        values.push(value);
+    }
+
+    return values;
+}
+
+function GetInitializeData() {
+    var values = [];
+    for (var i = 0; i <= 1000; i++) {
+        var x = i / (1000) * 2 * Math.PI;
+        var y = null;
+        var value = { x: x, y: y };
         values.push(value);
     }
 
@@ -33,9 +45,10 @@ function InitializeGraph() {
             backgroundColor: 'rgb(255, 99, 132)',
             hoverRadius: 1.5
         }, {
-            radius: 1.5,
+            radius: 0,
+            data: GetInitializeData(),
             order: 1,
-            showLine: false,
+            showLine: true,
             fill: false,
             borderColor: 'rgb(0, 0, 0)',
             backgroundColor: 'rgb(0, 0, 0)',
@@ -101,23 +114,20 @@ function ChangeGraph(chart, a1 = 0.5, a2 = 0.5, a5 = 0.5, a10 = 0.5, a20 = 0.5) 
     chart.update();
 }
 
-function GetHintData(a1, a5, a10, a25, a50) {
+function UpdateHintData(hintData, a1, a5, a10, a25, a50) {
     var answerFrourier = GetFrourier(answerCoefficients[0], answerCoefficients[1], answerCoefficients[2], answerCoefficients[3], answerCoefficients[4]);
     var checkFrourier = GetFrourier(a1, a5, a10, a25, a50);
 
     var hintValues = [];
-    for (var i = 0; i < answerFrourier.length; i++) {
+    for (var i = 0; i < hintData.length; i++) {
         if (Math.abs(answerFrourier[i].y - checkFrourier[i].y) < 0.05) {
             value = { x: answerFrourier[i].x, y: answerFrourier[i].y };
-            hintValues.push(value);
+            hintData[i] = value;
         }
     }
-
-    return hintValues;
 }
 
 function UpdateHintGraph(chart, a1 = 0.5, a2 = 0.5, a5 = 0.5, a10 = 0.5, a20 = 0.5) {
-    var hintDataSets = chart.data.datasets[1].data.concat(GetHintData(a1, a2, a5, a10, a20));
-    chart.data.datasets[1].data = hintDataSets;
+    UpdateHintData(chart.data.datasets[1].data, a1, a2, a5, a10, a20);
     chart.update();
 }
